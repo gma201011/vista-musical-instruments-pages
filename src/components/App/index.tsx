4c80684bc4c598946dcc24f3fa1d82b2d543b1dc
gma201011 <gma201011@hotmail.com>
2022-08-12 17:52:51 +0800

    Add global reset css

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,10 @@
+import { ResetStyle } from './ResetStyle';
 import ArtistPage from '../../pages/Artist';
 
 const App = () => {
   return (
     <div className='App'>
+      <ResetStyle />
       <ArtistPage />
     </div>
   );
